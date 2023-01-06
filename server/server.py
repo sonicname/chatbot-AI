8b7ea7a40e87187455ed7baa261e103bb7eb4473
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2023-01-06 09:41:14 +0700

    move from nltk to underthesea

--- server/server.py
+++ server/server.py
@@ -1,5 +1,4 @@
-import nltk
-from nltk.stem.lancaster import LancasterStemmer
+from underthesea import word_tokenize
 import numpy as np
 import tflearn
 import random
@@ -9,55 +8,44 @@ import json
 import socketio
 import eventlet
 
-stemmer = LancasterStemmer()
-
-data = pickle.load(open(r"models_save\training_data", "rb"))
+data = pickle.load(open("models_save/training_data", "rb"))
 words = data['words']
 classes = data['classes']
 train_x = data['train_x']
 train_y = data['train_y']
 
-with open(r"data\intents.json", encoding="utf8") as json_data:
+with open("data/intents.json", encoding="utf8") as json_data:
     intents = json.load(json_data)
 
 net = tflearn.input_data(shape=[None, len(train_x[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
+net = tflearn.regression(net)
 
 model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 
 
-def clean_up_sentence(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    return sentence_words
-
-
-# bag of words
-def bow(sentence, words, show_details=False):
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words
-    bag = [0] * len(words)
+def bow(sentence, word):
+    sentence_words = word_tokenize(sentence)
+    sentence_words = [word.lower() for word in sentence_words]
+    bag = [0] * len(word)
     for s in sentence_words:
-        for i, w in enumerate(words):
+        for i, w in enumerate(word):
             if w == s:
                 bag[i] = 1
-                if show_details:
-                    print("found in bag: %s" % w)
 
     return np.array(bag)
 
 
-model.load(r'models_save\model.tflearn')
+model.load('models_save/model.tflearn')
 
 context = {}
 
 ERROR_THRESHOLD = 0.25
 
 
-def classify(sentence):
+def classify_ai(sentence):
     results = model.predict([bow(sentence, words)])[0]
     results = [[i, r] for i, r in enumerate(results) if r > ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
@@ -67,18 +55,16 @@ def classify(sentence):
     return return_list
 
 
-def response(sentence, userID='1', show_details=False):
-    results = classify(sentence)
+def response(sentence, userID='1'):
+    results = classify_ai(sentence)
     if results:
         while results:
             for i in intents['intents']:
                 if i['tag'] == results[0][0]:
                     if 'context_set' in i:
-                        if show_details: print('context:', i['context_set'])
                         context[userID] = i['context_set']
                     if not 'context_filter' in i or \
                             (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
-                        if show_details: print('tag:', i['tag'])
                         return random.choice(i['responses'])
 
             results.pop(0)
@@ -105,9 +91,8 @@ def connect_error():
 
 @io.event
 def message(sid, data):
-    print(classify(data))
-    response_message = response(data)
-    io.emit("response", response_message)
+    print(classify_ai(data))
+    io.emit("response", response(data))
 
 
 if __name__ == "__main__":
--- server/train.py
+++ server/train.py
@@ -1,5 +1,4 @@
-import nltk
-from nltk.stem.lancaster import LancasterStemmer
+from underthesea import word_tokenize
 import numpy as np
 import tflearn
 import tensorflow as tf
@@ -7,39 +6,8 @@ import random
 import json
 import pickle
 
-stemmer = LancasterStemmer()
-nltk.download('punkt')
-
 with open('data/intents.json', encoding="utf8") as json_data:
     intents = json.load(json_data)
-
-
-def ngrams(str, n):
-    tokens = str.split(' ')
-    arr = []
-    for i in range(len(tokens)):
-        new_str = ''
-        if i == 0 and n > 1:
-            new_str = '_'
-            for j in range(n):
-                if j < n - 1:
-                    if (i + j) <= len(tokens):
-                        new_str += ' ' + tokens[i + j]
-                    else:
-                        new_str += ' _'
-        else:
-            for j in range(n):
-                if j < n:
-                    if (i + j) < len(tokens):
-                        if j == 0:
-                            new_str += tokens[i + j]
-                        else:
-                            new_str += ' ' + tokens[i + j]
-                    else:
-                        new_str += ' _'
-        arr.append(new_str)
-    return arr
-
 
 words = []
 classes = []
@@ -48,27 +16,24 @@ ignore_words = ['?', 'và', 'à', 'ừ', 'ạ', 'vì', 'từng', 'một_cách']
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-
-        w = nltk.word_tokenize(pattern)
+        w = word_tokenize(pattern)
         words.extend(w)
         documents.append((w, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+words = [w.lower() for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-
 classes = sorted(list(set(classes)))
 
 training = []
 output = []
-
 output_empty = [0] * len(classes)
 
 for doc in documents:
     bag = []
     pattern_words = doc[0]
-    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    pattern_words = [word.lower() for word in pattern_words]
 
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
@@ -90,7 +55,7 @@ net = tflearn.input_data(shape=[None, len(train_x[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
+net = tflearn.regression(net)
 
 model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 
